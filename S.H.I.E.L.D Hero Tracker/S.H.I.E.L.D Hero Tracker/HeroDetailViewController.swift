86db2d646d26db960e3dbf2a4146da9ed9923af8
Michael Reynolds <reynoldsmichael945@gmail.com>
2015-10-12 14:57:00 -0400

    Yak Shaving

---
+++ S.H.I.E.L.D Hero Tracker/S.H.I.E.L.D Hero Tracker/HeroDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HeroDetailViewController.swift
+//  S.H.I.E.L.D Hero Tracker
+//
+//  Created by Michael Reynolds on 10/12/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class HeroDetailViewController: UIViewController
+{
+
+}
