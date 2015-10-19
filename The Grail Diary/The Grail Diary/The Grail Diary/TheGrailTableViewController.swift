2202bde858d99a32b6ed6009c19e256142ece1af
Michael Reynolds <reynoldsmichael945@gmail.com>
2015-10-19 16:21:11 -0400

    Added Site Class

--- The Grail Diary/The Grail Diary/The Grail Diary/TheGrailTableViewController.swift
+++ The Grail Diary/The Grail Diary/The Grail Diary/TheGrailTableViewController.swift
@@ -33,24 +33,24 @@ class TheGrailTableViewController: UITableViewController
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int
     {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 10
     }
 
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TheGrail", forIndexPath: indexPath) as! Site
 
         // Configure the cell...
+        
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
