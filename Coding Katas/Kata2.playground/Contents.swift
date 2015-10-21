62b4a2c7fa2f36158829dcaa807aa6051008e46e
Michael Reynolds <reynoldsmichael945@gmail.com>
2015-10-21 09:59:20 -0400

    Question 1 Complete

--- Coding Katas/Kata2.playground/Contents.swift
+++ Coding Katas/Kata2.playground/Contents.swift
@@ -1,6 +1,26 @@
 //: ## Question 1
 //: ### Leap Year
 //: You are given a year, determine if it’s a leap year. A leap year is a year containing an extra day. It has 366 days instead of the normal 365 days. The extra day is added in February, which has 29 days instead of the normal 28 days. Leap years occur every 4 years. 2012 is a leap year and so is 2016. Except that every 100 years special rules apply. Years that are divisible by 100 are not leap years if they are not divisible by 400. For example 1900 was not a leap year, but 2000 was. __Print Leap year! or Not a leap year! depending on the case.__
+let year = 2016
+if year % 4 == 0
+{
+    if year % 100 == 0 && year % 400 != 0
+    {
+        print("Not a leap year!")
+    }
+    else
+    {
+        print("Leap year!")
+    }
+}
+else
+{
+    print("Not a leap year!")
+}
+    
+
+
+
 
 
 
@@ -9,7 +29,11 @@
 //: You are working on a smart-fridge. The smart-fridge knows how old the eggs and bacon in it are. You know that eggs spoils after 3 weeks (21 days) and bacon after one week (7 days). Given ```baconAge``` and ```eggsAge``` (in days) determine if you can cook bacon and eggs or what ingredients you need to throw out.
 //:
 //: If you can cook bacon and eggs print you can cook bacon and eggs. If you need to throw out any ingredients for each one print a line with the text throw out ingredient (throw out bacon or throw out eggs) in any order.
+let baconAge = 7
+let eggsAge = 21
 
+print("you can cook bacon and eggs")
+print("throw out ingredient")
 
 
 //: ## Question 3
