ab6fb7ef436584c1dc10a66c35c31a379841e403
Michael Reynolds <reynoldsmichael945@gmail.com>
2015-10-28 10:25:14 -0400

    Some Progress

--- GitHub Friends/GitHub Friends/FriendListTableViewController.swift
+++ GitHub Friends/GitHub Friends/FriendListTableViewController.swift
@@ -8,19 +8,45 @@
 
 import UIKit
 
-class FriendListTableViewController: UITableViewController
+protocol APIControllerProtocol
+{
+    func didReceiveAPIResults(results: NSArray)
+}
+
+class FriendListTableViewController: UITableViewController, APIControllerProtocol
 {
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        api = APIController(delegate: self)
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "FriendCell")
+
     }
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int
+    {
+        1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        10
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("FriendCell", forIndexPath: indexPath)
+        
+        return cell
     }
 
 
