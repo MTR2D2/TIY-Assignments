5315fe8c094bbcd87a7168a4a40fba0d379b1daa
Michael Reynolds <reynoldsmichael945@gmail.com>
2015-11-11 13:38:09 -0500

    Yak Shaving

---
+++ HighVoltageObjectiveC/HighVoltageObjectiveC/ValuesTypesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ValuesTypesTableViewController.h
+//  HighVoltageObjectiveC
+//
+//  Created by Michael Reynolds on 11/11/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ValuesTypesTableViewController : UITableViewController
+
+@end
---
+++ HighVoltageObjectiveC/HighVoltageObjectiveC/VoltageCalculatorTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VoltageCalculatorTableViewController.h
+//  HighVoltageObjectiveC
+//
+//  Created by Michael Reynolds on 11/11/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VoltageCalculatorTableViewController : UITableViewController
+
+@end
