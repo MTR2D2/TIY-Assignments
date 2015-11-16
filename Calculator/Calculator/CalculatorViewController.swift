25831a20cf96514ec8d29fc7b9a231810c7c6f6d
Michael Reynolds <reynoldsmichael945@gmail.com>
2015-11-16 15:53:20 -0500

    Merge branch 'Calculator'
    # Conflicts:
    #	Coding Katas/Kata1.playground/timeline.xctimeline
    #	Coding Katas/Kata1.playground/timeline.xctimeline~Jackpot
    #	Coding
    Katas/Kata2.playground/playground.xcworkspace/contents.xcworkspacedata
    #	Coding Katas/Kata2.playground/timeline.xctimeline
    #	Human.playground/playground.xcworkspace/contents.xcworkspacedata
    #	Human.playground/timeline.xctimeline
    #	Human/Human.playground/playground.xcworkspace/contents.xcworkspacedata
    #	Human/Human.playground/timeline.xctimeline
    #
    Jackpot/Jackpot.xcodeproj/project.xcworkspace/xcuserdata/Michael.xcuserd
    atad/UserInterfaceState.xcuserstate
    #	Jackpot/Jackpot/Base.lproj/Main.storyboard
    #	Kata.playground/timeline.xctimeline

---
+++ Calculator/Calculator/CalculatorViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Michael Reynolds on 10/15/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController
+{
+    var brain = CalculatorBrain()
+
+    @IBOutlet weak var Display: UILabel!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Action Handlers
+    
+    @IBAction func Operators(sender: UIButton)
+    {
+        brain.assignSymbol(sender.currentTitle!)
+    }
+    
+    @IBAction func Clear(sender: UIButton)
+    {
+        Display.text = "0"
+        brain = CalculatorBrain()
+    }
+
+    @IBAction func Input(sender: UIButton)
+    {
+        Display.text = brain.assignOperand(sender.currentTitle!)
+         
+// When the sender UIButton selects an input number currentTitle, the result of calling the assignOperand function from the BrainCalculator class updates the display.text
+    }
+
+    @IBAction func Calculate(sender: UIButton)
+    {
+        Display.text = brain.calculateAnswer(sender.currentTitle!)
+    }
+}
+
---
+++ HeroTracker/S.H.I.E.L.D Hero Tracker/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.swift
+//  S.H.I.E.L.D Hero Tracker
+//
+//  Created by Michael Reynolds on 11/4/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
