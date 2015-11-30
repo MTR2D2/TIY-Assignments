3d645865f47d6d3e91522bbbc65540b40bcb2d47
Michael Reynolds <reynoldsmichael945@gmail.com>
2015-11-30 14:03:41 -0500

    Initial submit

---
+++ VenueMenu/VenueMenu/Entity+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Entity+CoreDataProperties.swift
+//  VenueMenu
+//
+//  Created by Michael Reynolds on 11/29/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Entity {
+
+    @NSManaged var favorite: NSNumber?
+
+}
---
+++ VenueMenu/VenueMenu/Entity.swift
@@ -0,0 +1,17 @@
+//
+//  Entity.swift
+//  VenueMenu
+//
+//  Created by Michael Reynolds on 11/29/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Entity: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
